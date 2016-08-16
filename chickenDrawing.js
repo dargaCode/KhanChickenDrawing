4ea25cf0b0c8a7aadba31af4deb47f3aa58ca588
dargaCode <darga.code@gmail.com>
2016-08-15 17:37:07 -0700

    Embed Khan JS code into HTML

--- chickenDrawing.js
+++ chickenDrawing.js
@@ -1,57 +1,70 @@
-noStroke();
-background(110, 216, 245);
 
-//grass
-fill(146, 209, 0);
-rect(0,152,400,441);
+var sketchProc=function(processingInstance){ with (processingInstance){
 
-//feet
-fill(250, 188, 53);
+  size(400, 400);
+  frameRate(30);
 
-//left foot
-triangle(161,254,187,206,186,263);
-ellipse(163,250,18,17);
-ellipse(171,261,18,17);
-ellipse(185,257,18,17);
 
-//right foot
-triangle(209,211,222,259,243,250);
-ellipse(222,254,18,17);
-ellipse(234,258,18,17);
-ellipse(240,249,18,17);
+  noStroke();
+  background(110, 216, 245);
 
-//body
-fill(254, 255, 242);
-ellipse(201,180,107,108);
+  //grass
+  fill(146, 209, 0);
+  rect(0,152,400,441);
 
-//head
-ellipse(199,116,64,55);
+  //feet
+  fill(250, 188, 53);
 
-//left wing
-arc(141,173,103,100,166,374);
-ellipse(99,176,22,44);
-ellipse(119,173,22,44);
-ellipse(138,170,22,44);
+  //left foot
+  triangle(161,254,187,206,186,263);
+  ellipse(163,250,18,17);
+  ellipse(171,261,18,17);
+  ellipse(185,257,18,17);
 
-//right wing
-arc(263,168,105,100,160,370);
-ellipse(264,163,22,44);
-ellipse(285,167,22,44);
-ellipse(305,173,22,44);
+  //right foot
+  triangle(209,211,222,259,243,250);
+  ellipse(222,254,18,17);
+  ellipse(234,258,18,17);
+  ellipse(240,249,18,17);
 
-//beak
-fill(250, 188, 53);
+  //body
+  fill(254, 255, 242);
+  ellipse(201,180,107,108);
 
-ellipse(199,125,27,12);
-triangle(200,144,186,126,208,125);
-triangle(191,125,207,143,212,125);
-ellipse(203,141,8,9);
+  //head
+  ellipse(199,116,64,55);
 
-//eyes
-fill(21, 69, 128);
-ellipse(190,111,7,11);
-ellipse(205,109,7,11);
+  //left wing
+  arc(141,173,103,100,3,6);
+  ellipse(99,176,22,44);
+  ellipse(119,173,22,44);
+  ellipse(138,170,22,44);
 
-//fan
-fill(255, 0, 0);
-ellipse(196,85,18,32);
+  //right wing
+  arc(263,168,105,100,3,6.5);
+  ellipse(264,163,22,44);
+  ellipse(285,167,22,44);
+  ellipse(305,173,22,44);
+
+  //beak
+  fill(250, 188, 53);
+
+  ellipse(199,125,27,12);
+  triangle(200,144,186,126,208,125);
+  triangle(191,125,207,143,212,125);
+  ellipse(203,141,8,9);
+
+  //eyes
+  fill(21, 69, 128);
+  ellipse(190,111,7,11);
+  ellipse(205,109,7,11);
+
+  //fan
+  fill(255, 0, 0);
+  ellipse(196,85,18,32);
+
+}};
+
+var canvas = document.getElementById('myCanvas');
+// attaching the sketchProc function to the canvas
+var processingInstance = new Processing(canvas, sketchProc);
